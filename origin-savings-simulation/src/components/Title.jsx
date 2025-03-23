0ccfc9c6661584697f2a4c00b6245773058d7010
Osama <md.osama.rao@gmail.com>
2025-03-23 23:54:11 +0100

    matching title fixes w.r.t figma

--- origin-savings-simulation/src/components/Title.jsx
+++ origin-savings-simulation/src/components/Title.jsx
@@ -6,7 +6,7 @@
 function Title() {
   return (
     <div className="w-full flex justify-center bg-[#F4F8FA]">
-      <h1 className="text-center mt-[80px] mb-6 text-base font-medium text-[#1B31A8] max-w-[560px] px-4">
+      <h1 className="text-center mt-12 mb-4 text-base font-medium text-[#1B31A8] max-w-[560px] px-4">
         Let’s plan your <span className="font-semibold text-[#1B31A8]">saving goal.</span>
       </h1>
     </div>
